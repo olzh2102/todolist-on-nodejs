6c88353baf0ead879fafd537b6be0601aedabe9a
olzhas2102 <olzhas2102@yahoo.com>
2018-03-14 19:07:30 +0100

    Creating module to handle routing

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var todoController = require('./controllers/todoController');
+var app = express();
+
+// Setup template engine 
+app.set('view engine', 'ejs');
+
+// Static files 
+app.use(express.static('./public'));
+
+// Fire controllers 
+todoController(app);
+
+// Listen to port 
+app.listen(3000);
+console.log("You are listening to port 3000");
---
+++ controllers/todoController.js
@@ -0,0 +1,14 @@
+module.export = function(app) {
+
+  app.get('/todo', function(req, res) {
+
+  });
+
+  app.post('/todo', function(req, res) {
+    
+  });
+
+  app.delete('/todo', function(req, res) {
+    
+  });
+};
