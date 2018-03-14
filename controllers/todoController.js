8907339f1b40929309fb5a075ad7ebde92b60432
olzhas2102 <olzhas2102@yahoo.com>
2018-03-14 22:54:08 +0100

    Connecting mongoDB with mlab and mongoose - testing with dummy data

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,20 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+// Connect to DB 
+mongoose.connect('mongodb://test:test@ds113799.mlab.com:13799/todo-olly');
+
+// Create a schema - this is like a blueprint 
+var todoSchema = new mongoose.Schema({
+  task: String
+});
+
+// Doing model 
+var Todo = mongoose.model('Todo', todoSchema); //model name
+var taskOne = Todo({task: 'get flowers'}).save(function(err) {
+  if(err) throw err;
+  console.log('task saved');
+});
 
 var data = [{item: 'get milk'},{item: 'cook dinner'},{item: 'watch UCL'}];
 
