78f0bdc0d53a715325f48de6aaa5c2ab50031a1c
olzhas2102 <olzhas2102@yahoo.com>
2018-03-14 19:27:27 +0100

    Creating template to view

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,7 +1,8 @@
-module.export = function(app) {
+module.exports = function(app) {
 
+  // To view app whenever user enters
   app.get('/todo', function(req, res) {
-
+    res.render('todo');
   });
 
   app.post('/todo', function(req, res) {
