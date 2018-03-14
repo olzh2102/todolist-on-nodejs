6a1db33ffda96873f4fb13864a4acff4547e9f98
olzhas2102 <olzhas2102@yahoo.com>
2018-03-14 23:23:54 +0100

    Posting, getting and deleting items to from mongoDB

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -6,17 +6,20 @@ mongoose.connect('mongodb://test:test@ds113799.mlab.com:13799/todo-olly');
 
 // Create a schema - this is like a blueprint 
 var todoSchema = new mongoose.Schema({
-  task: String
+  item: String
 });
 
-// Doing model 
+// Doing model - collection
 var Todo = mongoose.model('Todo', todoSchema); //model name
-var taskOne = Todo({task: 'get flowers'}).save(function(err) {
-  if(err) throw err;
-  console.log('task saved');
-});
 
-var data = [{item: 'get milk'},{item: 'cook dinner'},{item: 'watch UCL'}];
+// ----- DUMMY DATA to TEST -----
+// var taskOne = Todo({task: 'get flowers'}).save(function(err) {
+//   if(err) throw err;
+//   console.log('task saved');
+// });
+
+//var data = [{item: 'get milk'},{item: 'cook dinner'},{item: 'watch UCL'}];
+//var data = [];
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
@@ -24,18 +27,26 @@ module.exports = function(app) {
 
   // To view app whenever user enters
   app.get('/todo', function(req, res) {
-    res.render('todo', {todos: data});
+    // Get data from mongoDB and pass it to view 
+    Todo.find({}, function(err, data) {
+      if(err) throw err;
+      res.render('todo', {todos: data});
+    }); // Retrieve all the tasks   
   });
 
   app.post('/todo', urlencodedParser, function(req, res) {
-    data.push(req.body);
-    res.json(data);
+    // Get data from the view and add it to mongoDB 
+    var newTodo = Todo(req.body).save(function(err,data) {
+      if(err) throw err;
+      res.json(data);
+    })
   });
 
   app.delete('/todo/:item', function(req, res) {
-    data = data.filter(function(todo) {
-      return todo.item.replace(/ /g, '-') !== req.params.item;
+    // Delete requested task from mongoDB
+    Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data) {
+      if(err) throw err;
+      res.json(data);
     });
-    res.json(data);
   });
 };
